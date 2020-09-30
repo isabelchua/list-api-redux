ae3566816e05fa6cd631332310b0e3f10d606c25
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-09-30 09:50:36 -0700

    connected user to post

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,3 +10,8 @@ export const fetchPosts = () => async dispatch => {
 	// 	payload: response
 	// };
 };
+
+export const fetchUser = id => async dispatch => {
+	const response = await jsonPlaceholder.get(`/users/${id}`);
+	dispatch({ type: "FETCH_USER", payload: response.data });
+};
--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,9 +1,25 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchUser } from "../actions";
 
 class UserHeader extends Component {
+	componentDidMount() {
+		this.props.fetchUser(this.props.userId);
+	}
+
 	render() {
-		return <div>User header</div>;
+		//const user = this.props.users.find(user => user.id === this.props.user.Id);
+		const { user } = this.props;
+		if (!user) {
+			return null;
+		}
+
+		return <div className="header">{user.name}</div>;
 	}
 }
 
-export default UserHeader;
+const mapStateToProps = (state, ownProps) => {
+	return { user: state.users.find(user => user.id === ownProps.userId) };
+};
+
+export default connect(mapStateToProps, { fetchUser })(UserHeader);
